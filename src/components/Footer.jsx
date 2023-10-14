69f28a6a79ed229e7fecec10411d359097468dcd
nousra0 <raissouninossra@gmail.com>
2023-10-14 22:35:46 +0300

    deploy web

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
         <div className='flex-1 flex flex-col justify-start mr-10'>
-          <img src={logo} alt="bank" className='w-[266px] h-[72px] object-contain' />
+          {/* <img src={logo} alt="bank" className='w-[266px] h-[72px] object-contain' /> */}
+          <h1 className='text-white'>Logo</h1>
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
         </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      {/* <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' /> */}
+      <h1 className='text-white'>Logo</h1>
       <ul className='list-none flex sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index)=> (
           <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
