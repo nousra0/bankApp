c3bf280426e739b398d82794b3234c4f5a55fccf
nousra <travelshopturkey58@gmail.com>
2023-10-14 13:17:30 +0300

    first commit

---
+++ src/components/Business.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { features } from '../constants'
+import styles, {layout} from '../style'
+import Button from './Button'
+
+const Business = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Business
