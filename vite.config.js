b8978b6926f1710a2b459b8e92ef851e8c11e27a
nousra0 <raissouninossra@gmail.com>
2023-10-15 21:13:23 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bank-app/',
+  base: '/bankApp/',
   build: {
     assetsDir: 'assets'
   },
